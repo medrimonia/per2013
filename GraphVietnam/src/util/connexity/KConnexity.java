88a314446d89547ad5c8fec66f4ae176a13f8b14
Thibaud Lambert <Thibaud.Lambert@gmail.com>
2013-11-30 16:58:22 +0100

    Add generator base on degree

--- GraphVietnam/src/util/connexity/KConnexity.java
+++ GraphVietnam/src/util/connexity/KConnexity.java
@@ -1,5 +1,7 @@
 package util.connexity;
 
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Set;
 
 import util.Graphs;
@@ -12,10 +14,10 @@ public class KConnexity {
 	/**
 	 * 	Generate a k-connected graph with two complete components linked by k edge
 	 * 	@param sizeComponent The size of two components
-	 * 	@param k The connexity of the graph
+	 * 	@param k The connectivity of the graph
 	 *  @return The k-connected generated graph
 	 * */
-	static public Graph<Integer, Graph.Edge<Integer>> generateKConnectedGraphWithTwoComponent(int sizeComponent, int k)
+	static public Graph<Integer, Graph.Edge<Integer>> genTwoComponent(int sizeComponent, int k)
 	{
 		Graph<Integer, Graph.Edge<Integer>> g = new MultiGraph<Integer, Graph.Edge<Integer>>();
 		int size = sizeComponent*2;
@@ -43,10 +45,71 @@ public class KConnexity {
 	
 	
 	/**
+	 * 	Generate a k-connected graph
+	 * 	@param size The graph size
+	 * 	@param k The connectivity of the graph
+	 *  @return The k-connected generated graph
+	 * */
+	static public Graph<Integer, Graph.Edge<Integer>> genBaseOnDegree(int size, int k)
+	{
+		Graph<Integer, Graph.Edge<Integer>> g = new MultiGraph<Integer, Graph.Edge<Integer>>();
+		
+		/*Adding the vertices */
+		for (int i = 0; i < size; i++)
+			g.addVertex(i);
+		
+		Random rand = new Random();
+		
+		ArrayList<int[]> edgeList = new ArrayList<int[]>();
+		for(int x = 0; x < size; x++)
+		{
+			for(int y = x+1; y < size; y++)
+			{
+				int[] edge = new int[2];
+				edge[0] = x;
+				edge[1] = y;
+				edgeList.add(edge);
+			}
+		}
+		
+		/*Generate the two complete components*/
+		for (int i = 0; i < size; i++)
+		{
+			int j = g.degree(i);
+			while(j < k)
+			{
+				int r = rand.nextInt(size-j-1);
+				
+				for(int[] edge : edgeList)
+				{
+					if(edge[0] == i || edge[1] == i)
+					{
+						if(r == 0)
+						{
+							g.addEdge(new DirectedEdge<Integer>(edge[0],edge[1]));
+							edgeList.remove(edge);
+							j++;
+							break;
+						}
+						else
+						{
+							r--;
+						}
+					}
+				}
+			}
+			
+		}
+		
+		return g;
+	}
+	
+	
+	/**
 	 * Is the graph given is parameter is k-connected
 	 * @param g The graph to test
-	 * @param k The k-connexity to test
-	 * @return true if the graph connexity is greater than k else false
+	 * @param k The k-connectivity to test
+	 * @return true if the graph connectivity is greater than k else false
 	 */
 	static public <V, E extends Graph.Edge<V>> boolean isKConnected(Graph<V, E> g, int k)
 	{
@@ -56,11 +119,11 @@ public class KConnexity {
 	
 	
 	/**
-	 * Compute the level of connexity of the graph
+	 * Compute the level of connectivity of the graph
 	 * @param g The graph to compute on
-	 * @return The level of connexity
+	 * @return The level of connectivity
 	 */
-	static public <V, E extends Graph.Edge<V>> int connexityLevel(Graph<V, E> g)
+	static public <V, E extends Graph.Edge<V>> int connectivityLevel(Graph<V, E> g)
 	{
 		Set<V> minCutSet = Graphs.minimumCutset(g);
 		return minCutSet.size();
